7308cdc05cf142de8f28096517e50797a9bac2cd
Sajad Abbasi <sajjadintel@gmail.com>
2020-12-28 19:13:16 +0330

    move search API to Product List API

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -6,7 +6,6 @@ class ProductController extends MainController {
     public function getProducts()
     {
         $limit = 10;
-
         if (isset($_GET['limit']))
             $limit = (int)$_GET['limit'];
         $order['limit'] = $limit;
@@ -19,12 +18,21 @@ class ProductController extends MainController {
         $sortBy = 'id_desc';
         if (isset($_GET['sort']))
             $sortBy = $_GET['sort'];
-
         $order['order'] = $sortBy;
+
+        $queryParam = null;
+        if (!isset($_GET['search']))
+            $queryParam = $_GET['search'];
 
 
         $filter = "";
 
+        if ($queryParam !== null) {
+            $filter = " scientificName LIKE '%{$queryParam}%'";
+            $filter .= " OR productName_ar LIKE '%{$queryParam}%'";
+            $filter .= " OR productName_en LIKE '%{$queryParam}%'";
+            $filter .= " OR productName_fr LIKE '%{$queryParam}%'";
+        }
 
         switch ($sortBy) {
             case "rand":
@@ -164,130 +172,6 @@ class ProductController extends MainController {
         $data['bonus'] = $arrBonus;
 
         $this->sendSuccess(Constants::HTTP_OK, $this->f3->get('RESPONSE.200_listFound', $this->f3->get('RESPONSE.entity_bonus')), $data);
-    }
-
-    public function getSearchResults()
-    {
-        if (!isset($_GET['query']))
-            $this->sendError(Constants::HTTP_FORBIDDEN, $this->f3->get('RESPONSE.400_paramMissing', $this->f3->get('RESPONSE.entity_query')), null);
-        $queryParam = $_GET['query'];
-
-
-        $limit = 10;
-        if (isset($_GET['limit']))
-            $limit = (int)$_GET['limit'];
-        $order['limit'] = $limit;
-
-        $offset = 0;
-        if (isset($_GET['offset']))
-            $offset = (int)$_GET['offset'];
-        $order['offset'] = $offset;
-
-        $sortBy = 'id_desc';
-        if (isset($_GET['sort']))
-            $sortBy = $_GET['sort'];
-        $order['order'] = $sortBy;
-
-
-        $filter = " scientificName LIKE '%{$queryParam}%'";
-        $filter .= " OR productName_ar LIKE '%{$queryParam}%'";
-        $filter .= " OR productName_en LIKE '%{$queryParam}%'";
-        $filter .= " OR productName_fr LIKE '%{$queryParam}%'";
-
-        switch ($sortBy) {
-            case "rand":
-                $orderString = "rand()";
-                break;
-
-            case "id_asc":
-                $orderString = "id ASC";
-                break;
-            case "id_desc":
-                $orderString = "id DESC";
-                break;
-
-            case "product_name_asc":
-                $orderString = "productName_en ASC, id ASC";
-                break;
-            case "product_name_desc":
-                $orderString = "productName_en DESC, id ASC";
-                break;
-
-            case "scientific_name_asc":
-                $orderString = "scientificName ASC, id ASC";
-                break;
-            case "scientific_name_desc":
-                $orderString = "scientificName DESC, id ASC";
-                break;
-
-            case "unit_price_asc":
-                $orderString = "unitPrice ASC, id ASC";
-                break;
-            case "unit_price_desc":
-                $orderString = "unitPrice DESC, id ASC";
-                break;
-
-            case "vat_asc":
-                $orderString = "vat ASC, id ASC";
-                break;
-            case "vat_desc":
-                $orderString = "vat DESC, id ASC";
-                break;
-
-            case "stock_status_name_asc":
-                $orderString = "stockStatusName_en ASC, id ASC";
-                break;
-            case "stock_status_name_desc":
-                $orderString = "stockStatusName_en DESC, id ASC";
-                break;
-
-            case "stock_asc":
-                $orderString = "stock ASC, id ASC";
-                break;
-            case "stock_desc":
-                $orderString = "stock DESC, id ASC";
-                break;
-
-            case "stock_updated_asc":
-                $orderString = "stockUpdateDateTime ASC, id ASC";
-                break;
-            case "stock_updated_desc":
-                $orderString = "stockUpdateDateTime DESC, id ASC";
-                break;
-
-            case "made_in_country_name_asc":
-                $orderString = "madeInCountryName_en ASC, id ASC";
-                break;
-            case "made_in_country_name_desc":
-                $orderString = "madeInCountryName_en DESC, id ASC";
-                break;
-
-            default:
-                $this->sendError(Constants::HTTP_BAD_REQUEST, $this->f3->get('RESPONSE.400_paramInvalid', $this->f3->get('RESPONSE.entity_Sort')), null);
-                return;
-        }
-        $order['order'] = $orderString;
-
-
-        $dbProducts = new GenericModel($this->db, "vwEntityProductSell");
-        $dbProducts->productName = "productName_" . $this->language;
-        $dbProducts->entityName = "entityName_" . $this->language;
-        $dbProducts->bonusTypeName = "bonusTypeName_" . $this->language;
-        $dbProducts->madeInCountryName = "madeInCountryName_" . $this->language;
-
-        $dataCount = $dbProducts->count($filter);
-        $dbProducts->reset();
-
-        $dataFilter = new stdClass();
-        $dataFilter->dataCount = $dataCount;
-        $dataFilter->filter = $filter;
-        $dataFilter->order = $order;
-
-        $response['dataFilter'] = $dataFilter;
-
-        $response['data'] = array_map(array($dbProducts, 'cast'), $dbProducts->find($filter, $order));
-
-        $this->sendSuccess(Constants::HTTP_OK, $this->f3->get('RESPONSE.200_listFound', $this->f3->get('RESPONSE.entity_product')), $response);
     }
 
 }
