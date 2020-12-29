4ebddbaad95ce74da596907836c7c5a600b730a3
Sajad Abbasi <sajjadintel@gmail.com>
2020-12-29 13:01:36 +0330

    add get news type list

--- app/controllers/NewsController.php
+++ app/controllers/NewsController.php
@@ -90,5 +90,76 @@ class NewsController extends MainController {
         $this->sendSuccess(Constants::HTTP_OK, $this->f3->get('RESPONSE.200_detailFound', $this->f3->get('RESPONSE.entity_news')), $response);
     }
 
+    public function getNewsTypeList()
+    {
+        $limit = 10;
+        if (isset($_GET['limit']))
+            $limit = (int)$_GET['limit'];
+        $order['limit'] = $limit;
+
+        $offset = 0;
+        if (isset($_GET['offset']))
+            $offset = (int)$_GET['offset'];
+        $order['offset'] = $offset;
+
+        $sortBy = 'id_desc';
+        if (isset($_GET['sort']))
+            $sortBy = $_GET['sort'];
+        $order['order'] = $sortBy;
+
+
+        $filter = "";
+
+        switch ($sortBy) {
+            case "rand":
+                $orderString = "rand()";
+                break;
+
+            case "id_asc":
+                $orderString = "id ASC";
+                break;
+            case "id_desc":
+                $orderString = "id DESC";
+                break;
+
+            case "title_asc":
+                $orderString = "title_en ASC, id ASC";
+                break;
+            case "title_desc":
+                $orderString = "title_en DESC, id ASC";
+                break;
+
+            case "description_asc":
+                $orderString = "description_en ASC, id ASC";
+                break;
+            case "description_desc":
+                $orderString = "description_en DESC, id ASC";
+                break;
+
+            default:
+                $this->sendError(Constants::HTTP_BAD_REQUEST, $this->f3->get('RESPONSE.400_paramInvalid', $this->f3->get('RESPONSE.entity_Sort')), null);
+                return;
+        }
+        $order['order'] = $orderString;
+
+
+        $dbNews = new GenericModel($this->db, "newsType");
+        $dbNews->name = 'name_' . $this->language;
+
+        $dataCount = $dbNews->count($filter);
+        $dbNews->reset();
+
+        $dataFilter = new stdClass();
+        $dataFilter->dataCount = $dataCount;
+        $dataFilter->filter = $filter;
+        $dataFilter->order = $order;
+
+        $response['dataFilter'] = $dataFilter;
+
+        $response['data'] = array_map(array($dbNews, 'cast'), $dbNews->find($filter, $order));
+
+        $this->sendSuccess(Constants::HTTP_OK, $this->f3->get('RESPONSE.200_listFound', $this->f3->get('RESPONSE.entity_newsType')), $response);
+    }
+
 
 }
--- routes.php
+++ routes.php
@@ -52,3 +52,4 @@ $f3->route('GET /v1/pharmacy/sellers', 'SearchController->getSellerList');
 ## News Endpoints
 $f3->route('GET /v1/pharmacy/news', 'NewsController->getNewsList');
 $f3->route('GET /v1/pharmacy/news/@id', 'NewsController->getNews');
+$f3->route('GET /v1/pharmacy/newsType', 'NewsController->getNewsTypeList');
