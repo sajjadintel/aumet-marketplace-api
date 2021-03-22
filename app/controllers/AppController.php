1381d137efd0399e1504e5600f78023307824b3f
Sajad Abbasi <sajjadintel@gmail.com>
2021-03-22 20:51:09 +0430

    Worked on MPA-51 fixed

--- app/controllers/AppController.php
+++ app/controllers/AppController.php
@@ -1,7 +1,6 @@
 <?php
 
-class AppController extends MainController
-{
+class AppController extends MainController {
     function beforeRoute()
     {
         $this->beforeRouteFunction();
@@ -15,9 +14,18 @@ class AppController extends MainController
         $res->appName = getenv("APP_NAME");
         $res->appVersion = getenv("APP_VERSION");
 
+//        $dbSetting = new GenericModel($this->db, "setting");
+//        $settings = $dbSetting->findWhere("language = '{$this->language}'");
+//        $res->settings = $settings;
+
         $dbSetting = new GenericModel($this->db, "setting");
-        $settings = $dbSetting->findWhere("language = '{$this->language}'");
-        $res->settings = $settings;
+        $dbSetting->getWhere("language = '{$this->language}'");
+
+        while (!$dbSetting->dry()) {
+            $res->settings[$dbSetting->title] = $dbSetting->value;
+            $dbSetting->next();
+        }
+
 
         $this->sendSuccess(Constants::HTTP_OK, $this->f3->get('RESPONSE.200_detailFound', $this->f3->get('RESPONSE.entity_appSettings')), $res);
     }
