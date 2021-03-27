77db80e95bcf3054fa4833040c0fd9dbfd5898c3
Sajad Abbasi <sajjadintel@gmail.com>
2021-03-28 03:37:46 +0400

    Updated api request log

--- app/classes/ApiRequestsLog.php
+++ app/classes/ApiRequestsLog.php
@@ -3,19 +3,29 @@
 class ApiRequestsLog
 {
 
-    public static function logRequest($f3, $db, $userId, $sessionId, $data, $type, $ip)
+    public static function logRequest($f3, $db, $userId, $sessionId, $data, $ip)
     {
-
         $dbLog = new GenericModel($db, "apiRequestLog");
 
         $dbLog->userId = $userId;
         $dbLog->sessionId = $sessionId;
-        $dbLog->type = $type;
+        $dbLog->type = Constants::LOG_TYPE_INIT;
         $dbLog->request =  $f3->get('PARAMS.0');
         $dbLog->data = $data;
         $dbLog->ip = $ip;
 
         // TODO: Add error handling
-        $dbLog->add();
+        $dbLog->addReturnID();
+
+        return $dbLog;
+    }
+
+    public static function updateRequestResponse($dbRequest, $type, $response)
+    {
+        $dbRequest->type = $type;
+        $dbRequest->response = $response;
+
+        // TODO: Add error handling
+        $dbRequest->update();
     }
 }
