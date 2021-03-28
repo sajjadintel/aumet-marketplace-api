96cfbf256292b956bc3e2097f8a2165b19e1833b
Sajad Abbasi <sajjadintel@gmail.com>
2021-03-28 20:10:49 +0400

    Updated distributor API

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -15,6 +15,11 @@ class SearchController extends MainController
             $offset = (int)$_GET['offset'];
         $order['offset'] = $offset;
 
+        $sortBy = 'id_desc';
+        if (isset($_GET['sort']))
+            $sortBy = $_GET['sort'];
+        $order['order'] = $sortBy;
+
         $search = null;
         if (isset($_GET['search']) && $_GET['search'] != "")
             $search = $_GET['search'];
@@ -27,6 +32,32 @@ class SearchController extends MainController
             $filter .= " OR name_fr LIKE '%{$search}%'";
             $filter .= " OR name_ar LIKE '%{$search}%' ) ";
         }
+
+        switch ($sortBy) {
+            case "rand":
+                $orderString = "rand()";
+                break;
+
+            case "id_asc":
+                $orderString = "id ASC";
+                break;
+            case "id_desc":
+                $orderString = "id DESC";
+                break;
+
+            case "name_asc":
+                $orderString = "name_en ASC, id ASC";
+                break;
+            case "name_desc":
+                $orderString = "name_en DESC, id ASC";
+                break;
+
+            default:
+                $this->sendError(Constants::HTTP_BAD_REQUEST, $this->f3->get('RESPONSE.400_paramInvalid', $this->f3->get('RESPONSE.entity_Sort')), null);
+                return;
+        }
+
+        $order['order'] = $orderString;
 
         $dbProducts = new GenericModel($this->db, "entity");
         $dbProducts->name = 'name_' . $this->language;
